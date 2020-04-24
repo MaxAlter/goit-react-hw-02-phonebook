b235dc7a2b4dfdabf5dc804eb2295adb5a907f66
MaxAlter <alter0075926@gmail.com>
2020-04-24 19:09:59 +0300

    Updates

---
+++ precache-manifest.c76ce9c984a6513823562103169f43de.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "920461ab9baf2a0f97de3c2d6ccf7ac0",
+    "url": "/goit-react-hw-02-phonebook/index.html"
+  },
+  {
+    "revision": "fe3959b6adfe58a2cf3c",
+    "url": "/goit-react-hw-02-phonebook/static/css/main.62fd7cf7.chunk.css"
+  },
+  {
+    "revision": "132ad777a2726185d6fd",
+    "url": "/goit-react-hw-02-phonebook/static/js/2.a310acc5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/goit-react-hw-02-phonebook/static/js/2.a310acc5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fe3959b6adfe58a2cf3c",
+    "url": "/goit-react-hw-02-phonebook/static/js/main.31c3f700.chunk.js"
+  },
+  {
+    "revision": "55a68c81338df23b9fff",
+    "url": "/goit-react-hw-02-phonebook/static/js/runtime-main.7a75d1a0.js"
+  }
+]);
